f27c8145edb07c31312aad905e10b132eb92fc6d
zeynepturnali <zeynepturnali23@gmail.com>
2020-09-09 19:52:55 +0300

    protect key and token

--- src/main/java/GeneralEnvironment.java
+++ src/main/java/GeneralEnvironment.java
@@ -1,6 +1,6 @@
 public class GeneralEnvironment {
     String trelloAPIURL = "https://api.trello.com";
-    String trelloKey = "8ad7dd68c4fa44cb3644453a230edcbf";
-    String trelloToken = "639c08724757cb1069c2542a77554a0f853638c50f5c59ec19513aee1a84248b";
+    String trelloKey = "your trello key";
+    String trelloToken = "your trello token";
 
 }
